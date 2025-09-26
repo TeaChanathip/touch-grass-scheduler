b5da250257752393df1432c7d2ddb68d4e2c5709
TeaChanathip <teachanthip@gmail.com>
2025-09-26 16:58:32 +0700

    [Add] docker compose and init file for db

---
+++ db/init.sql
@@ -0,0 +1,139 @@
+CREATE TYPE gender AS ENUM('Male', 'Female', 'Other', 'Prefer not to say');
+
+-- Entity
+CREATE TABLE IF NOT EXISTS "students" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "first_name" VARCHAR(128) NOT NULL,
+    "middle_name" VARCHAR (128) DEFAULT NULL,
+    "last_name" VARCHAR(128) DEFAULT NULL,
+    "phone" VARCHAR(15) NOT NULL,
+    "gender" gender NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+    "avatar_url" VARCHAR(512) DEFAULT NULL,
+    "student_id" VARCHAR(16) NOT NULL,
+    UNIQUE("email")
+);
+
+CREATE TABLE IF NOT EXISTS "teachers" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "first_name" VARCHAR(128) NOT NULL,
+    "middle_name" VARCHAR (128) DEFAULT NULL,
+    "last_name" VARCHAR(128) DEFAULT NULL,
+    "phone" VARCHAR(15) NOT NULL,
+    "gender" gender NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+    "avatar_url" VARCHAR(512) DEFAULT NULL,
+    "teacher_id" VARCHAR(16) NOT NULL,
+    UNIQUE("email")
+);
+
+CREATE TABLE IF NOT EXISTS "guardians" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "first_name" VARCHAR(128) NOT NULL,
+    "middle_name" VARCHAR (128) DEFAULT NULL,
+    "last_name" VARCHAR(128) DEFAULT NULL,
+    "phone" VARCHAR(15) NOT NULL,
+    "gender" gender NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+    "avatar_url" VARCHAR(512) DEFAULT NULL,
+    UNIQUE("email")
+);
+
+CREATE TABLE IF NOT EXISTS "admins" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "first_name" VARCHAR(128) NOT NULL,
+    "middle_name" VARCHAR (128) DEFAULT NULL,
+    "last_name" VARCHAR(128) DEFAULT NULL,
+    "phone" VARCHAR(15) NOT NULL,
+    "gender" gender NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL
+    "avatar_url" VARCHAR(512) DEFAULT NULL,
+    UNIQUE("email")
+);
+
+CREATE TABLE IF NOT EXISTS "classes" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(128) NOT NULL,
+    "student_count" INTEGER DEFAULT 0,
+    "school_id" UUID NOT NULL REFERENCES "schools"("id") ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "schools" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(128) NOT NULL,
+    "class_count" INTEGER DEFAULT 0,
+    "building_num" VARCHAR(16) NOT NULL,
+    "moo" SMALLINT DEFAULT NULL,
+    "soi" VARCHAR(32) DEFAULT NULL,
+    "road" VARCHAR(32) DEFAULT NULL,
+    "sub_district" VARCHAR(32) DEFAULT,
+    "district" VARCHAR(32) DEFAULT,
+    "province" VARCHAR(32) DEFAULT
+);
+
+CREATE TABLE IF NOT EXISTS "homework" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(128) NOT NULL,
+    "man_hours" NUMERIC(3, 2) NOT NULL,
+    "description" VARCHAR(1024) DEFAULT NULL,
+    "score" DOUBLE PRECISION DEFAULT NULL,
+    "book_id" UUID DEFAULT NULL REFERENCES "books"("id") ON DELETE CASCADE
+);  
+
+CREATE TABLE IF NOT EXISTS "books" (
+    "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "title" VARCHAR(128) NOT NULL,
+    "page_count" INTEGER NOT NULL,
+    "excercise_count" INTEGER NOT NULL,
+    "total_man_hours" NUMERIC(4, 2) NOT NULL,
+);
+
+-- Relation
+
+CREATE TYPE relationship_type as ENUM('Mother', 'Father', 'Others')
+
+CREATE TABLE IF NOT EXISTS "student_guardians" (
+    "student_id" UUID NOT NULL REFERENCES "students"("id") ON DELETE CASCADE,
+    "guardian_id" UUID NOT NULL REFERENCES "guardians"("id") ON DELETE CASCADE,
+    "type" relationship_type NOT NULL,
+    PRIMARY KEY ("student_id", "guardian_id")
+);
+
+CREATE TABLE IF NOT EXISTS "class_teachers" (
+    "class_id" UUID NOT NULL REFERENCES "classes"("id") ON DELETE CASCADE,
+    "teacher_id" UUID NOT NULL REFERENCES "teachers"("id") ON DELETE CASCADE,
+    PRIMARY KEY ("class_id", "teacher_id")
+);
+
+CREATE TABLE IF NOT EXISTS "class_students" (
+    "class_id" UUID NOT NULL REFERENCES "classes"("id") ON DELETE CASCADE,
+    "student_id" UUID NOT NULL REFERENCES "students"("id") ON DELETE CASCADE,
+    PRIMARY KEY ("class_id", "student_id")
+);
+
+CREATE TABLE IF NOT EXISTS "homework_teachers" (
+    "homework_id" UUID NOT NULL REFERENCES "homework"("id") ON DELETE CASCADE,
+    "teacher_id" UUID NOT NULL REFERENCES "teachers"("id") ON DELETE CASCADE,
+    PRIMARY KEY ("homework_id", "teacher_id")
+);
+
+CREATE TABLE IF NOT EXISTS "homework_students" (
+    "homework_id" UUID NOT NULL REFERENCES "homework"("id") ON DELETE CASCADE,
+    "student_id" UUID NOT NULL REFERENCES "students"("id") ON DELETE CASCADE,
+    "score" DOUBLE PRECISION DEFAULT NULL,
+    PRIMARY KEY ("homework_id", "student_id")
+);
+
+CREATE TABLE IF NOT EXISTS "assignments" (
+    "teacher_id" UUID NOT NULL REFERENCES "teachers"("id") ON DELETE CASCADE,
+    "class_id" UUID NOT NULL REFERENCES "classes"("id") ON DELETE CASCADE,
+    "homework_id" UUID NOT NULL REFERENCES "homework"("id") ON DELETE CASCADE,
+    "created_at" TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    "assigned_at" TIMESTAMPTZ DEFAULT NULL,
+    "due_at" TIMESTAMPTZ DEFAULT NULL,
+    PRIMARY KEY ("teacher_id", "class_id", "homework_id")
+);
