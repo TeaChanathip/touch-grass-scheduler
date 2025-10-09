1e1f2039079ddbc0162b1ca849768d0f8e76b5c1
TeaChanathip <teachanthip@gmail.com>
2025-10-10 02:29:43 +0700

    [Client][Add] Setup client using NextJS with bun

---
+++ client/eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: import.meta.dirname,
+    recommendedConfig: js.configs.recommended,
+});
+
+const eslintConfig = [
+    {
+        ignores: [".next/**", "node_modules/**", "out/**", "build/**"],
+    },
+    ...compat.config({
+        extends: ["eslint:recommended", "next"],
+    }),
+];
+
+export default eslintConfig;
